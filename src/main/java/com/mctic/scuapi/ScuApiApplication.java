f144d120e25491a8418878de9617cb06aad4ad7b
Manoel Albino Neto <manoelneto841@yahoo.com.br>
2020-06-09 19:44:14 -0300

    first commit

---
+++ src/main/java/com/mctic/scuapi/ScuApiApplication.java
@@ -0,0 +1,13 @@
+package com.mctic.scuapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScuApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScuApiApplication.class, args);
+	}
+
+}
